5531b1642ee3058c7b9a1e16e1f2cb702d277ea9
Antonio Thomacelli <tonnytg@gmail.com>
2021-12-06 22:53:09 -0300

    feat: add new method business rules

--- entity/products/products.go
+++
@@ -1,9 +0,0 @@
-package products
-
-type Product struct {
-	ID          int64    `json:"id"`
-	Name        string   `json:"name"`
-	Description string   `json:"description"`
-	Price       float64  `json:"price"`
-	Actions     []string `json:"actions"`
-}
